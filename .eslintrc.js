cfdaf0d4132e1e1725e9c5ed4c310201038080fa
OptimalRating <optimalrating@gmail.com>
2025-05-22 08:23:19 +0000

    last updation on es lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,45 @@
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:vue/vue3-recommended",
-    "plugin:prettier/recommended"
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2021,
-    sourceType: "module"
+    sourceType: 'module',
   },
   plugins: [
-    "vue",
-    "prettier"
+    'vue',
+    'prettier',
   ],
   rules: {
-    "vue/multi-word-component-names": "off",
-    "vue/no-mutating-props": "warn",
-    "vue/no-unused-components": "warn",
-    "vue/valid-template-root": "warn",
-    "no-undef": "warn",
-    "prettier/prettier": "error",
-    "semi": ["error", "always"],
-    "comma-dangle": ["error", "always-multiline"],
-    "space-before-function-paren": ["error", "never"],
-    "arrow-parens": ["error", "always"],
-    "require-jsdoc": ["error", {
+    // Vue specific rules
+    'vue/multi-word-component-names': 'off',
+    'vue/no-mutating-props': 'warn',
+    'vue/no-unused-components': 'warn',
+    'vue/valid-template-root': 'warn',
+
+    // General JavaScript rules
+    'no-undef': 'warn',
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-empty-function': 'warn',
+
+    // Prettier and formatting
+    'prettier/prettier': 'error',
+    'semi': ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'space-before-function-paren': ['error', 'never'],
+    'arrow-parens': ['error', 'always'],
+
+    // JSDoc (optional)
+    'require-jsdoc': ['error', {
       require: {
         FunctionDeclaration: false,
         MethodDefinition: false,
-        ClassDeclaration: false
-      }
+        ClassDeclaration: false,
+      },
     }],
-    // Miscellaneous tweaks
-    'no-console': 'warn', // Warn about console logs (can be adjusted to 'off' or 'error' if needed)
-    'no-debugger': 'warn', // Warn about debugger statements
-    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], // Warn on unused variables but allow unused function arguments with an underscore
-    'no-empty-function': 'warn', // Warn about empty functions
-  }
+  },
 };
