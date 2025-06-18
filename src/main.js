341b76746cb088bf9bbb5be9be6522b995c50278
OptimalRating <optimalrating@gmail.com>
2025-06-18 10:10:30 +0000

    installed ant design

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from "vue";
 import plugins from "./plugins";
 import App from "./App.vue";
 import store from "./store";
+// ✅ Import Ant Design Vue
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+
+// ✅ Use the plugin
+Vue.use(Antd);
 
 store.dispatch("auth/check");
 
