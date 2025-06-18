471bc46c23f7c72a7f1c4dfc2687696583169da2
OptimalRating <optimalrating@gmail.com>
2025-06-18 09:57:17 +0000

    updated change password functionality

--- src/components/Dashboard/Views/ChangePassword.vue
+++ src/components/Dashboard/Views/ChangePassword.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div class="content">
     <div class="container-fluid">
       <div class="row">
@@ -13,19 +13,19 @@
               <div class="form-group row">
                 <label class="col-4 col-form-label">Old Password:</label>
                 <div class="col-8">
-                  <input type="text" class="form-control" />
+                  <input type="password" name="oldPassword" class="form-control" />
                 </div>
               </div>
               <div class="form-group row">
                 <label class="col-4 col-form-label">New Password:</label>
                 <div class="col-8">
-                  <input type="text" class="form-control" />
+                  <input type="password" name="password" class="form-control" />
                 </div>
               </div>
               <div class="form-group row">
                 <label class="col-4 col-form-label">Confirm Password:</label>
                 <div class="col-8">
-                  <input type="text" class="form-control" />
+                  <input type="password" name="confirm" class="form-control" />
                 </div>
               </div>
               <button
@@ -44,5 +44,109 @@
 </template>
 <script>
 import Card from "src/components/UIComponents/Cards/Card.vue";
-export default {};
+export default {
+
+};
+</script> -->
+
+<template>
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-8">
+          <h4 class="mb-4 mt-2">Change Password</h4>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <card>
+            <form @submit.prevent="onSubmit">
+              <div class="form-group row">
+                <label class="col-4 col-form-label">Old Password:</label>
+                <div class="col-8">
+                  <input v-model="form.oldPassword" type="password" name="oldPassword" class="form-control" />
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-4 col-form-label">New Password:</label>
+                <div class="col-8">
+                  <input v-model="form.password" type="password" name="password" class="form-control" />
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-4 col-form-label">Confirm Password:</label>
+                <div class="col-8">
+                  <input v-model="form.confirm" type="password" name="confirm" class="form-control" />
+                </div>
+              </div>
+              <button
+                type="submit"
+                class="btn btn-sm btn-fill btn-round btn-primary float-right mb-2 mt-2"
+                :disabled="loading"
+              >
+                {{ loading ? 'Saving...' : 'Save' }}
+              </button>
+            </form>
+          </card>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from "src/components/UIComponents/Cards/Card.vue";
+import { notification } from "ant-design-vue"; // or any notification plugin you're using
+// import ApiUrl from "@/api/apiUrl"; // base API URL
+import axios from "axios"; // Axios is the most common HTTP client
+
+export default {
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      form: {
+        oldPassword: '',
+        password: '',
+        confirm: '',
+      },
+      loading: false,
+    };
+  },
+  methods: {
+    async post(url, data) {
+      return axios.post(url, data);
+    },
+    async onSubmit() {
+      if (this.form.password !== this.form.confirm) {
+        notification.error({ message: "Passwords do not match" });
+        return;
+      }
+
+      this.loading = true;
+      try {
+        await this.post(`${'https://server.optimalrating.com/api/'}check-password`, { password: this.form.oldPassword });
+        await this.post(`${'https://server.optimalrating.com/api/'}password-change`, { password: this.form.password });
+
+        this.loading = false;
+        this.form.oldPassword = '';
+        this.form.password = '';
+        this.form.confirm = '';
+
+        notification.success({ message: 'Password has been updated.' });
+      } catch (error) {
+        this.loading = false;
+
+        const errorMessage =
+          error?.response?.config?.url.includes("check-password")
+            ? 'Old password is wrong'
+            : 'Password could not be changed.';
+
+        notification.error({ message: errorMessage });
+      }
+    }
+  }
+};
 </script>
+
